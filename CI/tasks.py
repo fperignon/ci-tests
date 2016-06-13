a4362025811be63151db9a75ffd7007b22038c8b
Franck Pérignon <franck.perignon@imag.fr>
2016-06-13 16:21:18 +0200

    rien

--- CI/tasks.py
+++ CI/tasks.py
@@ -3,7 +3,7 @@ from machinery.ci_task import CiTask
 citests_default = CiTask(
     ci_config='default',
     distrib='debian:latest',
-    #pkgs=['build-base', 'gcc', 'python-env'],
+    pkgs=['build-base'],#, 'gcc', 'python-env'],
     srcs=['.'],
     #targets={'.': ['docker-build', 'docker-ctest']}
     )
