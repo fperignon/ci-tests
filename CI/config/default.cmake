83c75c99b0f3e07eeec8bf144b47373bc1beed3c
Franck Pérignon <franck.perignon@imag.fr>
2016-06-13 14:08:15 +0200

    test ci

---
+++ CI/config/default.cmake
@@ -0,0 +1,8 @@
+# default configuration for testing
+
+IF(NOT __DEFAULT_CMAKE__)
+  SET(__DEFAULT_CMAKE__ TRUE)
+  set_option(DEV_MODE ON)
+  set_option(WITH_TESTING ON)
+  set_option(WITH_SYSTEM_INFO ON)
+ENDIF(NOT __DEFAULT_CMAKE__)
