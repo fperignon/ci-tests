da1249f664a00922e94a39d2d7e5e6feeaeb625c
Franck Pérignon <franck.perignon@imag.fr>
2016-06-13 16:17:31 +0200

    rien

--- CI/tasks.py
+++ CI/tasks.py
@@ -2,10 +2,11 @@ from machinery.ci_task import CiTask
 
 citests_default = CiTask(
     ci_config='default',
-    distrib='ubuntu:14.04',
-    pkgs=['build-base', 'gcc', 'python-env'],
+    distrib='debian:latest',
+    #pkgs=['build-base', 'gcc', 'python-env'],
     srcs=['.'],
-    targets={'.': ['docker-build', 'docker-ctest']})
+    #targets={'.': ['docker-build', 'docker-ctest']}
+    )
 # dispatch based on hostname and distrib type (to min. disk requirement)
 
 known_tasks = {'citests---vm0':
