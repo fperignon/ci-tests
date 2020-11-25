e6197c2a13197afc4cb9a222a8192c67c56ce5ee
Franck Pérignon <franck.perignon@univ-grenoble-alpes.fr>
2020-11-25 15:30:28 +0100

    test

--- CI/before_install.sh
+++ CI/before_install.sh
@@ -2,19 +2,5 @@
 
 if test "$TRAVIS" = true; then 
     pip install --user PyYAML
-    sudo apt-get -o Acquire::CompressionTypes::Order::=bz2 update
-    sudo apt-get -y install software-properties-common
-#    sudo add-apt-repository ppa:smspillaz/cmake-2.8.12 -y
-    sudo apt-get update -qq
-#    sudo apt-get purge cmake -qq
-    sudo apt-get -y install make
-    sudo free -m
-    echo "test cmake version"
-    cmake --version
-    wget http://www.cmake.org/files/v3.0/cmake-3.0.2.tar.gz
-    tar xf cmake-3.0.2.tar.gz
-    cd cmake-3.0.2
-    ./configure
-    make install
 
 fi
